5bed66bc04df81ec891fd86ce2a38cfd99d3202a
muunoooo <mnaufal12321@gmail.com>
2024-11-13 15:49:29 +0700

    update

--- company-profile/src/app/contact/page.tsx
+++ company-profile/src/app/contact/page.tsx
@@ -1,10 +1,5 @@
-import ContactUs from "@/components/contact/contact";
 import React from "react";
 
 export default function Contact() {
-  return (
-    <div>
-      <ContactUs />
-    </div>
-  );
+  return <div></div>;
 }
