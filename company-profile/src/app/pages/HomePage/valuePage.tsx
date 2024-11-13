0eaaf4e864ffe869488e598ff322894162a1be35
muunoooo <mnaufal12321@gmail.com>
2024-11-13 16:05:30 +0700

    update

--- company-profile/src/app/pages/HomePage/valuePage.tsx
+++ company-profile/src/app/pages/HomePage/valuePage.tsx
@@ -14,21 +14,21 @@ export default function ValuePage() {
             </p>
           </div>
           <div className="text-start pl-7 flex flex-col gap-3">
-            <h3 className="text-2xl md:text-3xl text-[#c2f0fa] font-extrabold">
+            <p className="text-2xl md:text-3xl text-[#c2f0fa] font-extrabold">
               1. Best Service
-            </h3>
+            </p>
             <p className="md:text-xl md:pl-7 md:mb-4 text-[#e0f4f8]">
               We provide technology-based solutions with an emphasis on safety.
             </p>
-            <h3 className="text-2xl md:text-3xl text-[#c2f0fa] font-extrabold">
+            <p className="text-2xl md:text-3xl text-[#c2f0fa] font-extrabold">
               2. Embracing Change
-            </h3>
+            </p>
             <p className="md:text-xl md:pl-7 md:mb-4 text-[#e0f4f8]">
               We offer reasonable prices with high-quality standards.
             </p>
-            <h3 className="text-2xl md:text-3xl text-[#c2f0fa] font-extrabold">
+            <p className="text-2xl md:text-3xl text-[#c2f0fa] font-extrabold">
               3. Acting with Integrity
-            </h3>
+            </p>
             <p className="md:text-xl md:pl-7 md:mb-4 text-[#e0f4f8]">
               Proven geotechnical hydrology and mine impact online monitoring
               systems.
